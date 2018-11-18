8cb87e838267b1d4125e5978f0cd4dc20ee725ad
marioBytes <mario.bytes@gmail.com>
2018-11-18 11:40:33 -0700

    deleted email information

--- blog_final/settings.py
+++ blog_final/settings.py
@@ -134,14 +134,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
-# Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'andresmart92@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fifiatemywoolsox1'
-EMAIL_USE_TLS = True
-
 try:
   from .local_settings import *
 except ImportError:
